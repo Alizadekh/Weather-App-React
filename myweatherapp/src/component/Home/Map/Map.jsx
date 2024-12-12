73ff91351e5e12e7f0023bb238a73e2d48630d1d
Alizadekh <aykhanalizadekh@gmail.com>
2024-12-12 15:13:34 +0000

    Change Iframe settings

--- myweatherapp/src/component/Home/Map/Map.jsx
+++ myweatherapp/src/component/Home/Map/Map.jsx
@@ -29,13 +29,12 @@ function Map() {
   // console.log(lat);
   return (
     <div>
-      {lat && lng && (
-        <iframe
-          title='map'
-          src={`https://www.google.com/maps/embed/v1/view?key=${GEOCODING_API_KEY}&center=${lat},${lng}&zoom=12`}
-          allowFullScreen
-          loading='lazy'></iframe>
-      )}
+      <iframe
+        src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d194473.52377600502!2d49.69014863755424!3d40.39447550862524!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x40307d6bd6211cf9%3A0x343f6b5e7ae56c6b!2zQmFrxLE!5e0!3m2!1saz!2saz!4v1734016294481!5m2!1saz!2saz"
+        allowfullscreen=""
+        loading="lazy"
+        referrerpolicy="no-referrer-when-downgrade"
+      ></iframe>
     </div>
   );
 }
